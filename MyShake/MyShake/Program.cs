0f0357fc39da9a7b9886dc304b92c893c594fede
igor <sci.igor@gmail.com>
2016-04-03 00:06:49 +0300

    Класс "Змейка"

--- MyShake/MyShake/Program.cs
+++ MyShake/MyShake/Program.cs
@@ -21,8 +21,9 @@ namespace MyShake
             leftLine.Draw();
             rightLine.Draw();
             
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
         }
     }
 }
