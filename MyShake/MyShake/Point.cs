294897d527528b09ead63e7c1ca9e5a016dba747
igor <sci.igor@gmail.com>
2016-04-03 14:58:31 +0300

    Управляемая "Змейка"

--- MyShake/MyShake/Point.cs
+++ MyShake/MyShake/Point.cs
@@ -41,10 +41,10 @@ namespace MyShake
                 x = x - offset;
             }else if(direction == Direction.UP)
             {
-                y = y + offset;
+                y = y - offset;
             }else if(direction == Direction.DOWN)
             {
-                y = y - offset;
+                y = y + offset;
             }
         }
 
